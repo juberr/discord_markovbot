e77e00fd7dc3bca20981494a3e0c08c7ac514fbe
juberr <juberr1@live.ca>
2021-04-21 21:19:51 -0400

    further improvements to j!

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from markov_generator import generate
 import json 
 import requests
 import time
-
+import re
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -63,18 +63,22 @@ async def highlight(ctx):
 @client.command()
 @commands.cooldown(1, 11, commands.BucketType.user)
 async def jep(ctx):
+
+    valid_starts = ['what is', 'who is', 'when is', 'where is']
     
     # get a jeopardy question using the jservice.io
     jep = requests.get('http://jservice.io/api/random').json()
 
     # create a discord message to present question
     q =  f'''
-Topic: {jep[0]['category']['title']}
+```Topic: {jep[0]['category']['title']}
 
 Value: {jep[0]['value']}
 
 Question: {jep[0]['question']}
 
+ANSWER: REMOVE LATER {jep[0]['answer']}
+```
     '''
     print(q)
 
@@ -82,27 +86,38 @@ Question: {jep[0]['question']}
     await ctx.send(q)
 
     # begin the j! game
-    time_start = 0
+    time_start = time.time()
+
+    channel = client.get_channel(812424668320497717)
 
     while True:
-
-        if time_start == 10:
-            answer = jep[0]['answer']
-            await ctx.send(f'**Beep Beep Beep!** The answer was "{answer}"')
-            break
-        
-            
-        time.sleep(1)
-        time_start += 1
-
-        print(time_start)
-        
-        channel = client.get_channel(812424668320497717)
         
         prev_message = await channel.fetch_message(channel.last_message_id)
+
+        print(prev_message)
         
         if str(prev_message.author.name) != 'toesnshots.txt':
-            print(prev_message.content)
+            
+            usr_ans = str(prev_message.content)
+
+            usr_start = ' '.join(usr_ans.split()[0:2])
+
+            if usr_start in valid_starts:
+                valid_ans = ' '.join(usr_ans.split()[2:])
+                corr_answer = jep[0]['answer'].lower()
+                print(valid_ans)
+
+                if valid_ans == corr_answer:
+                    await ctx.send(f'grats {prev_message.author.mention}')
+                    break
+
+        if time.time() >= time_start + 15:
+            corr_answer = jep[0]['answer']
+            await ctx.send(f'**Beep Beep Beep!** The answer was "{corr_answer}"')
+            break        
+                
+            
+
 
 
 
